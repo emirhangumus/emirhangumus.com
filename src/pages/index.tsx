72a6e1ef4b750fe7a0f0792138560cb15e26cebd
EmirhanGumus <me@emirhangumus.com>
2023-04-17 23:02:10 +0300

    removed react-spring (for now at least)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,13 @@
 import FrontPageBanner from "@/components/FrontPageBanner";
-import { userAtom } from "@/lib/atoms/userAtom";
-import { useAtom } from "jotai";
 import Head from "next/head";
-import { animated } from "@react-spring/web";
 
 export default function Home() {
-
-  const [user, setUser] = useAtom(userAtom);
-
   return (
     <>
       <Head>
         <title>Anasayfa</title>
       </Head>
-      <animated.div>
-        <FrontPageBanner />
-      </animated.div>
+      <FrontPageBanner />
     </>
   )
 }
